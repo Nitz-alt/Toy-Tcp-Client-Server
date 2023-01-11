f3a5fc4cdd540ea802b830611af5ba14fb209e60
Nitzan Yizhar <nitzanyizhar@gmail.com>
2023-01-11 16:35:45 +0200

    Not working

--- pcc_client.c
+++ pcc_client.c
@@ -12,8 +12,12 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <assert.h>
+#include <signal.h>
 
 typedef ssize_t (*socket_op) (int, void *, size_t);
+#define ONE_MB 0x100000
+
 
 
 int read_from_socket(int socket_fd, char * buffer, size_t bytes_num){
@@ -21,7 +25,10 @@ int read_from_socket(int socket_fd, char * buffer, size_t bytes_num){
     int totalBytes = 0;
     while(1){
         bytes = read(socket_fd, buffer + totalBytes, bytes_num - totalBytes);
-        if (bytes == 0 || (bytes == -1 && (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE))){
+        if (bytes == 0){
+            return 0;
+        }
+        if ((bytes == -1 && (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE))){
             return -1;
         }
         totalBytes += bytes;
@@ -37,7 +44,10 @@ int write_to_socket(int socket_fd, char * buffer, size_t bytes_num){
     int totalBytes = 0;
     while(1){
         bytes = write(socket_fd, buffer + totalBytes, bytes_num - totalBytes);
-        if (bytes == 0 || (bytes == -1 && (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE))){
+        if (bytes == 0){
+            return 0;
+        }
+        if ((bytes == -1 && (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE))){
             return -1;
         }
         totalBytes += bytes;
@@ -53,27 +63,31 @@ int write_to_socket(int socket_fd, char * buffer, size_t bytes_num){
 
 int main(int argc, char *argv[])
 {
-    if (argc != 4){
-        fprintf(stderr, "Invalid arguments\n");
-        return 1;
-    }
-    int  sockfd     = -1;
+    int  sockfd;
     uint16_t port;
-    char *file_path, *file_memory;
+    char *file_path, file_buffer[ONE_MB];
     int fd;
     struct stat file_stat;
     char *buffer;
     uint32_t fileSize, printable_char;
     struct sockaddr_in serv_addr; // where we Want to get to
-    char *ip_addr = argv[1];
+    char *ip_addr;
     int reuse;
+    int totalBytes_sent;
+    int bytes_sent;
+    int current_bytes_num_read;
 
-
+    if (argc != 4){
+        fprintf(stderr, "Invalid arguments\n");
+        return 1;
+    }
+    signal(SIGINT, SIG_IGN);
+    ip_addr = argv[1];
     port = (uint16_t) atoi(argv[2]);
     file_path = argv[3];
     if (access(file_path, F_OK | R_OK) < 0){
-        perror("CLIENT: No permissions ");
-        return 1;
+        perror("CLIENT: No permissions to read file ");
+        return 0;
     }
 
     if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -87,20 +101,20 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(port); // Note: htons for endiannes
     if (inet_pton(AF_INET, ip_addr, &(serv_addr.sin_addr.s_addr)) < 0){
         perror("CLIENT: Failed at inet_pton ");
-        // close(sockfd);
+        close(sockfd);
         return 1;
     }
 
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *) &reuse, sizeof(int)) < 0){
         perror("CLIENT: Error at setting socket");
-        // close(sockfd);
-        exit(1);
+        close(sockfd);
+        return 1;
     }
 
     // connect socket to the target address
-    if(connect(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0){   
+    if (connect(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0){   
         perror("CLIENT: Connecting failed ");
-        // close(sockfd);
+        close(sockfd);
         return 1;
     }
     // Opening file to read
@@ -113,36 +127,46 @@ int main(int argc, char *argv[])
     // Getting file size
     if (fstat(fd, &file_stat) < 0){
         perror("CLIENT: Error at fstat ");
-        // close(sockfd);
-        // close(fd);
+        close(sockfd);
+        close(fd);
         return 1;
     }
 
     // Sending file size
     fileSize = htonl(file_stat.st_size);
     buffer = (char *) &fileSize;
-    if (write_to_socket(sockfd, buffer, 4) < 0){
+    if (write_to_socket(sockfd, buffer, 4) <= 0){
         perror("CLIENT: Failed at sending data to server ");
         return 1;
     }
 
     // Sending file data
-    file_memory = mmap(NULL, file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (write_to_socket(sockfd, file_memory, file_stat.st_size) < 0){
-        perror("CLIENT: Failed at sending data to server ");
-        munmap(file_memory, file_stat.st_size);
-        // close(sockfd);
-        // close(fd);
+    totalBytes_sent = 0;
+    bytes_sent = 1;
+    current_bytes_num_read = 0;
+    while (totalBytes_sent < file_stat.st_size){
+
+        current_bytes_num_read = read(fd, file_buffer, ONE_MB);
+        bytes_sent = write_to_socket(sockfd, file_buffer, current_bytes_num_read);
+        if (bytes_sent <= 0){
+            break;
+        }
+        assert(current_bytes_num_read == bytes_sent);
+        totalBytes_sent += bytes_sent;
+    }
+    if (bytes_sent <= 0){
+        perror("CLIENT: TCP Error occured");
+        close(sockfd);
+        close(fd);
         return 1;
     }
 
     // Getting # of printable characters
     buffer = (char *) &printable_char;
-    if (read_from_socket(sockfd, buffer, 4) < 0){
-        perror("CLIENT: Error reading data from server ");
-        // munmap(file_memory, file_stat.st_size);
-        // close(sockfd);
-        // close(fd);
+    if (read_from_socket(sockfd, buffer, 4) <= 0){
+        perror("CLIENT: TCP Error occured");
+        close(sockfd);
+        close(fd);
         return 1;
     }
     printable_char = ntohl(printable_char);
@@ -150,8 +174,7 @@ int main(int argc, char *argv[])
     // Printing # number of printable chars
     printf("# of printable characters: %u\n", printable_char);
     
-    munmap(file_memory, file_stat.st_size);
-    // close(fd);
-    // close(sockfd);
+    close(fd);
+    close(sockfd);
     return 0;
 }
--- pcc_server.c
+++ pcc_server.c
@@ -14,7 +14,7 @@
 #define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
 
 int EXIT = 0;
-uint32_t *pcc_total;
+uint32_t pcc_total[95];
 int listenfd, connfd;
 
 
@@ -35,7 +35,7 @@ int read_from_socket(int socket_fd, char * buffer, size_t bytes_num){
     int totalBytes = 0;
     while(1){
         bytes = read(socket_fd, buffer + totalBytes, bytes_num - totalBytes);
-        if (bytes == 0 ){
+        if (bytes == 0){
             return 0;
         }
         if ((bytes == -1 && (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE))){
@@ -54,7 +54,10 @@ int write_to_socket(int socket_fd, char * buffer, size_t bytes_num){
     int totalBytes = 0;
     while(1){
         bytes = write(socket_fd, buffer + totalBytes, bytes_num - totalBytes);
-        if (bytes == 0 || (bytes == -1 && (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE))){
+        if (bytes == 0){
+            return 0;
+        }
+        if ((bytes == -1 && (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE))){
             return -1;
         }
         totalBytes += bytes;
@@ -80,7 +83,7 @@ int main(int argc, char *argv[])
     struct sockaddr_in serv_addr;
     socklen_t addrsize = sizeof(struct sockaddr_in );
 
-    char *data_buff;
+    char data_buff[ONE_MB];
     uint16_t port;
     uint32_t file_size;
     ssize_t bytesRead, totalBytesRead;
@@ -88,7 +91,7 @@ int main(int argc, char *argv[])
     char charater;
     uint32_t total_printables;
     int reuse;
-    uint32_t *pcc_total_atm;
+    uint32_t pcc_total_atm[95];
     uint32_t current_bytes_read_count;
 
     struct sigaction sa_mid, sa_finish;
@@ -96,10 +99,13 @@ int main(int argc, char *argv[])
     port = atoi(argv[1]);
     connfd = -1;
 
-    sa_mid.sa_handler = setExit;
+    sa_mid.sa_flags = SA_RESTART;
+    sa_finish.sa_flags = SA_RESTART;
     sigemptyset(&sa_mid.sa_mask);
-    sa_finish.sa_handler = print_counts_and_exit;
     sigemptyset(&sa_finish.sa_mask);
+
+    sa_mid.sa_handler = setExit;
+    sa_finish.sa_handler = print_counts_and_exit;
 
     if (sigaction(SIGINT, &sa_finish, NULL) < 0){
         perror("SERVER: Failed at sigaction");
@@ -132,93 +138,93 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-
-    pcc_total = calloc(sizeof(uint32_t), 95);
-    pcc_total_atm = calloc(sizeof(uint32_t), 95);
-    data_buff = malloc(ONE_MB);
+    memset(pcc_total, 0, sizeof(uint32_t) * 95);
+    memset(pcc_total_atm, 0, sizeof(uint32_t) * 95);
+    memset(data_buff, 0, ONE_MB);
     while(1){
-        // Accept a connection.
-        connfd = accept( listenfd, NULL, NULL);
-
-        if( connfd < 0 ){
-            perror("SERVER: Error at accepting connection");
-            if (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE){
-                continue;
+        while(1){
+            // Accept a connection.
+            connfd = accept( listenfd, NULL, NULL);
+            if (sigaction(SIGINT, &sa_mid, NULL) < 0){
+                perror("SERVER: Failed at sigaction");
+                close(connfd);
+                close(listenfd);
+                return 1;
             }
-            close(listenfd);
-            exit(1);
-        }
+            if( connfd < 0 ){
+                perror("SERVER: Error at accepting connection");
+                if (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE){
+                    break;
+                }
+                close(listenfd);
+                exit(1);
+            }
+            // Reading number of bytes
+            buffer = (char *) &file_size;
+            if (read_from_socket(connfd, buffer, 4) <= 0){
+                perror("SERVER: TCP Error occured, read size");
+                close(connfd);
+                break;
+            }
+            file_size = ntohl(file_size);
 
-        if (sigaction(SIGINT, &sa_mid, NULL) < 0){
+            // Reading file data from client (in batches of 1MB)
+            bytesRead = 0;
+            totalBytesRead = 0;
+            buffer = data_buff;
+            total_printables = 0;
+            current_bytes_read_count = 0;
+            
+            while(totalBytesRead < file_size){
+                current_bytes_read_count = MIN(ONE_MB, file_size - totalBytesRead);
+                bytesRead = read_from_socket(connfd, buffer, current_bytes_read_count);
+                if (bytesRead <= 0){
+                    break;
+                }
+                // Check bytes for printable characters
+                for (int i = 0 ; i < bytesRead; i++){
+                    charater = buffer[i];
+                    if (IS_PRINTABLE_CHAR(charater)){
+                        pcc_total_atm[charater - 32]++;
+                        total_printables++;
+                    }
+                }
+                totalBytesRead += bytesRead;
+            }
+            if (bytesRead <= 0){
+                perror("SERVER: TCP Error occured, read file");
+                close(connfd);
+                break;
+            }
+
+            // Sending # of printables to the client
+            total_printables = htonl(total_printables);
+            buffer = (char *) &total_printables;
+            if (write_to_socket(connfd, buffer, 4) <= 0){
+                perror("SERVER: TCP Error occured, write printables count");
+                close(connfd);
+                break;
+            }
+
+            if (close(connfd) < -1){
+                break;
+            }
+
+            // Copying result
+            for (int i = 0; i < 95; i++){
+                pcc_total[i] += pcc_total_atm[i];
+            }
+            break;
+        }
+        memset(pcc_total_atm, 0, sizeof(uint32_t) * 95);
+        if (EXIT){
+                print_counts_and_exit(SIGINT);
+        }
+        if (sigaction(SIGINT, &sa_finish, NULL) < 0){
             perror("SERVER: Failed at sigaction");
             close(connfd);
             close(listenfd);
             return 1;
-        }
-        
-
-        // Reading number of bytes
-        buffer = (char *) &file_size;
-        if (read_from_socket(connfd, buffer, 4) <= 0){
-            perror("SERVER: Error at reading from client");
-            close(connfd);
-            continue;
-        }
-        file_size = ntohl(file_size);
-
-        // Reading file data from client (in batches of 1MB)
-        bytesRead = 0;
-        totalBytesRead = 0;
-        buffer = data_buff;
-        total_printables = 0;
-        current_bytes_read_count = 0;
-        
-        while(totalBytesRead < file_size){
-            current_bytes_read_count = MIN(ONE_MB, file_size - totalBytesRead);
-            bytesRead = read_from_socket(connfd, buffer, current_bytes_read_count);
-            if (bytesRead <= 0){
-                break;
-            }
-            // Check bytes for printable characters
-            for (int i = 0 ; i < bytesRead; i++){
-                charater = buffer[i];
-                if (IS_PRINTABLE_CHAR(charater)){
-                    pcc_total_atm[charater - 32]++;
-                    total_printables++;
-                }
-            }
-            totalBytesRead += bytesRead;
-        }
-        if (bytesRead <= 0){
-            perror("SERVER: TCP Error occured");
-            close(connfd);
-            continue;
-        }
-
-        // Sending # of printables to the client
-        total_printables = htonl(total_printables);
-        buffer = (char *) &total_printables;
-        if (write_to_socket(connfd, buffer, 4) <= 0){
-            perror("SERVER: TCP Error occured");
-            close(connfd);
-            continue;
-        }
-
-        // Copying result
-        for (int i = 0; i < 95; i++){
-            pcc_total[i] += pcc_total_atm[i];
-        }
-        memset(pcc_total_atm, 0, sizeof(uint32_t) * 95);
-        close(connfd);
-
-        if (sigaction(SIGINT, &sa_finish, NULL) < 0){
-            perror("SERVER: Error at setting signal handler");
-            close(connfd);
-            close(listenfd);
-            return 1;
-        }
-        if (EXIT){
-            print_counts_and_exit(SIGINT);
         }
     }
 }
